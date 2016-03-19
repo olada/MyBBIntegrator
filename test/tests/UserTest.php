3590adda2cdb4f225f839dd666683f3ea3a59d55
David Olah <david.olah@gmail.com>
2016-03-19 16:36:12 +0100

    tests for logging in which are depending on "immediate login feedback" which was integrated into last commit of MyBBIntegrator

--- test/tests/UserTest.php
+++ test/tests/UserTest.php
@@ -2,6 +2,9 @@
 
 class UserTest extends MyBBIntegratorTestCase {
 	
+	const NORMAL_USER_NAME = "normal";
+	const NORMAL_USER_PASSWORD = "normal123";
+
 	/**
 	 * Get list of members
 	 * Count should be exactly one - which is the admin user.
@@ -9,6 +12,25 @@ class UserTest extends MyBBIntegratorTestCase {
 	*/
 	public function testGetAllMembers() {
 		$members = $this->mybb_integrator->getMembers();
-		$this->assertEquals(1, count($members));
+		$this->assertEquals(2, count($members));
+	}
+
+	/**
+	 * Test if isLoggedIn state works after logging in as normal user
+	*/
+	public function testLoginAndLoggedInState() {
+		$this->assertFalse(
+			$this->mybb_integrator->isLoggedIn(),
+			"should be logged out before testing login"
+		);
+		$status = $this->mybb_integrator->login(self::NORMAL_USER_NAME, self::NORMAL_USER_PASSWORD);
+		$this->assertTrue(
+			$status,
+			"login as normal user should return true"
+		);
+		$this->assertTrue(
+			$this->mybb_integrator->isLoggedIn(),
+			"should be logged in after logging in as normal user"
+		);
 	}
 }
