b915f7d7ced4caf2990dbc737f32b93f2e00e883
David Olah <david.olah@gmail.com>
2016-03-19 17:58:14 +0100

    checking forum passwords test coverage

--- test/tests/ForumTest.php
+++ test/tests/ForumTest.php
@@ -1,11 +1,38 @@
 <?php
 
-class ForumTest extends \PHPUnit_Framework_TestCase {
+class ForumTest extends MyBBIntegratorTestCase {
+	
+	const PASSWORDED_FORUM_ID = 3; 
+	const NON_PASSWORDED_FORUM_ID = 2;
+
+	const WRONG_PASSWORD = "wrong_password";
+	const CORRECT_PASSWORD = "passworded";
+	
 	/**
-	 * @todo
 	 * Test for checking forum password
 	*/
-	public function testCheckForumPassword() {
+	public function testCheckForumPasswordForPasswordedForum() {
+		$this->assertFalse(
+			$this->mybb_integrator->checkForumPassword(self::PASSWORDED_FORUM_ID, self::WRONG_PASSWORD), 
+			"passworded forum should be false for wrong password"
+		);
+		$this->assertTrue(
+			$this->mybb_integrator->checkForumPassword(self::PASSWORDED_FORUM_ID, self::CORRECT_PASSWORD),
+			"passworded forum should be true for correct password"
+		);
+	}
 
+	/**
+	 * Testing that check for password for non-passworded forum is TRUE
+	*/
+	public function testCheckForumPasswordForNonPasswordedForum() {
+		$this->assertTrue(
+			$this->mybb_integrator->checkForumPassword(self::NON_PASSWORDED_FORUM_ID, self::WRONG_PASSWORD),
+			"non-passworded forum should be true for any password"
+		);
+		$this->assertTrue(
+			$this->mybb_integrator->checkForumPassword(self::NON_PASSWORDED_FORUM_ID, self::CORRECT_PASSWORD),
+			"non-passworded forum should be true for any password"
+		);
 	}
 }
