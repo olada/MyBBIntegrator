9cb61b22b4c0507d6755e6bb65dcf15ac88f6d3e
David Olah <david.olah@gmail.com>
2016-03-19 17:56:56 +0100

    use factory in parent test case

--- test/tests/MyBBIntegratorTestCase.php
+++ test/tests/MyBBIntegratorTestCase.php
@@ -2,15 +2,18 @@
 
 class MyBBIntegratorTestCase extends \PHPUnit_Framework_TestCase {
 	
+	private static $factory;
 	protected $mybb_integrator;
 
 	public function setUp() {
-		global $MyBBI;
-
-		$this->mybb_integrator =& $MyBBI;
+		$this->mybb_integrator = self::$factory->getInstance();
 	}
 
 	public function tearDown() {
+		unset($this->mybb_integrator);
+	}
 
+	public static function setFactory($mybb_integrator_factory) {
+		self::$factory = $mybb_integrator_factory;
 	}
 }
