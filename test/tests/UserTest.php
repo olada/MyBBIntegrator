88c206da402f8eb8d391bbe0d89021b024b1c253
David Olah <david.olah@gmail.com>
2016-03-20 14:23:59 +0100

    remove failure test

--- test/tests/UserTest.php
+++ test/tests/UserTest.php
@@ -33,8 +33,4 @@ class UserTest extends MyBBIntegratorTestCase {
 			"should be logged in after logging in as normal user"
 		);
 	}
-
-	public function testThisShouldFail() {
-		$this->assertFalse(true, "should fail");
-	}
 }
