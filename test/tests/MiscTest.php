3f9ca9050f572e9e7a69f4c59366575f31dcca08
David Olah <david.olah@gmail.com>
2016-04-07 21:15:17 +0200

    fix references to class constant

--- test/tests/MiscTest.php
+++ test/tests/MiscTest.php
@@ -5,21 +5,23 @@ class MiscTest extends MyBBIntegratorTestCase {
 	const USER_ID_FOR_POSTHASH_1 = 1;
 	const USER_ID_FOR_POSTHASH_2 = 2;
 
+	const MD5_LENGTH = 32;
+
 	public function testGeneratePostHashWithoutUserID() {
 		$posthash = $this->mybb_integrator->generatePosthash();
 		$posthash2 = $this->mybb_integrator->generatePosthash();
 		
-		$this->assertEquals(32, strlen($posthash));
-		$this->assertEquals(32, strlen($posthash2));
+		$this->assertEquals(self::MD5_LENGTH, strlen($posthash));
+		$this->assertEquals(self::MD5_LENGTH, strlen($posthash2));
 		$this->assertNotEquals($posthash, $posthash2);
 	}
 
 	public function testGeneratePostHashWithUserID() {
-		$posthash = $this->mybb_integrator->generatePosthash(USER_ID_FOR_POSTHASH_1);
-		$posthash2 = $this->mybb_integrator->generatePosthash(USER_ID_FOR_POSTHASH_2);
+		$posthash = $this->mybb_integrator->generatePosthash(self::USER_ID_FOR_POSTHASH_1);
+		$posthash2 = $this->mybb_integrator->generatePosthash(self::USER_ID_FOR_POSTHASH_2);
 		
-		$this->assertEquals(32, strlen($posthash));
-		$this->assertEquals(32, strlen($posthash2));
+		$this->assertEquals(self::MD5_LENGTH, strlen($posthash));
+		$this->assertEquals(self::MD5_LENGTH, strlen($posthash2));
 		$this->assertNotEquals($posthash, $posthash2);
 	}
 }
