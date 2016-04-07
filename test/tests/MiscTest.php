210100e215749bc78107b21c074303ff71840f28
David Olah <david.olah@gmail.com>
2016-04-07 21:07:36 +0200

    add generateposthash test for call w/ user id

--- test/tests/MiscTest.php
+++ test/tests/MiscTest.php
@@ -2,9 +2,21 @@
 
 class MiscTest extends MyBBIntegratorTestCase {
 
-	public function testGeneratePostHash() {
+	const USER_ID_FOR_POSTHASH_1 = 1;
+	const USER_ID_FOR_POSTHASH_2 = 2;
+
+	public function testGeneratePostHashWithoutUserID() {
 		$posthash = $this->mybb_integrator->generatePosthash();
 		$posthash2 = $this->mybb_integrator->generatePosthash();
+		
+		$this->assertEquals(32, strlen($posthash));
+		$this->assertEquals(32, strlen($posthash2));
+		$this->assertNotEquals($posthash, $posthash2);
+	}
+
+	public function testGeneratePostHashWithUserID() {
+		$posthash = $this->mybb_integrator->generatePosthash(USER_ID_FOR_POSTHASH_1);
+		$posthash2 = $this->mybb_integrator->generatePosthash(USER_ID_FOR_POSTHASH_2);
 		
 		$this->assertEquals(32, strlen($posthash));
 		$this->assertEquals(32, strlen($posthash2));
