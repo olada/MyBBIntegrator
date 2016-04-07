f0db0a2d102aacadfaf6d34b426dc1f948003f3a
David Olah <david.olah@gmail.com>
2016-04-07 20:49:49 +0200

    added test for generatePosthash

---
+++ test/tests/MiscTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class MiscTest extends MyBBIntegratorTestCase {
+
+	public function testGeneratePostHash() {
+		$posthash = $this->mybb_integrator->generatePosthash();
+		$posthash2 = $this->mybb_integrator->generatePosthash();
+		
+		$this->assertEquals(32, strlen($posthash));
+		$this->assertEquals(32, strlen($posthash2));
+		$this->assertEquals($posthash, $posthash2);
+	}
+}
